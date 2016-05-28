d1cd84657480721038733828d370520791428cb2
Thomas Weißschuh <thomas@t-8ch.de>
2016-05-28 12:48:04 +0000

    [lua] add get_nexthop()

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ all: ffffm ffffm.so ffffm-respondd.so ffffm-lua.so
 ffffm.so: LDLIBS+=-luci
 ffffm: LDLIBS+=-luci
 ffffm-lua.so: CFLAGS+=$(shell pkg-config --cflags lua5.1)
-ffffm-lua.so: LDLIBS+=$(shell pkg-config --libs lua5.1) -L.
+ffffm-lua.so: LDLIBS+=$(shell pkg-config --libs lua5.1) -L. ffffm.so
 
 %.so: %.c
 	$(CC) -shared -fPIC $(CFLAGS) $(LDLIBS) $< -o $@
--- ffffm-lua.c
+++ ffffm-lua.c
@@ -1,16 +1,31 @@
+#include <stdlib.h>
+
 #define LUA_LIB
 
 #include "lua.h"
 #include "lauxlib.h"
 
+#include "ffffm.h"
+
+static int get_nexthop(lua_State *L) {
+	char *n = ffffm_get_nexthop();
+
+	if (n) {
+		lua_pushstring(L, n);
+		free(n);
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
 static const luaL_Reg ffffmlib[] = {
-	{NULL, NULL}
+	{"get_nexthop", get_nexthop},
+	{NULL, NULL},
 };
 
 LUALIB_API int luaopen_ffffm (lua_State *L) {
 	luaL_register(L, "ffffm", ffffmlib);
-	lua_pushnumber(L, 15);
-	lua_setfield(L, -2, "test");
 	return 1;
 }
 
